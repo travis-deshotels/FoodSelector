b30d3582d9278954b381bcb0f0b3ad79c833cf46
Travis Deshotels <travis.deshotels@gmail.com>
2017-11-26 18:48:41 -0600

    Add input validation to add_food.

--- lambda/add_food.py
+++ lambda/add_food.py
@@ -4,16 +4,27 @@ import os
 
 def main(event, context):
     s3 = boto3.resource('s3')
+    if not is_valid_data(event):
+        return {'Error': 'Bad syntax! Please include name, restaurant, and choices!'}
 
     #write input data to file
     with open('/tmp/file', 'w') as outfile:
         json.dump(event, outfile)
 
-    #S3 put
+    #S3 put file
     try:
         s3.Object(os.environ['myBucket'], event['name'] + ".json").put(Body=open('/tmp/file', 'rb'))
     except:
         error_message = {'Error': 'Unable to save file'}
         return error_message
-    return_obj = {"message": "file saved"}
-    return return_obj
+    return {"message": "file saved"}
+
+def is_valid_data(event):
+    try:
+        print ("Input contains:")
+        print ("name" + event['name'])
+        print ("restaurant" + event['restaurant'])
+        print ("choices" + event['choices'])
+        return True
+    except (KeyError):
+        return False
