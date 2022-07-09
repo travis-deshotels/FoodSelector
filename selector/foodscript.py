26cf8de64bdb5c3124b7624c40f5bb3e3df7b12c
Travis Deshotels <travis.deshotels@gmail.com>
2022-07-09 09:25:52 -0500

    Add yaml support

--- selector/foodscript.py
+++ selector/foodscript.py
@@ -3,8 +3,8 @@ import sys
 import random
 
 
-def choose_food():
-    restaurant = random.choice(data)
+def choose_food(food_data):
+    restaurant = random.choice(food_data)
     print(restaurant['name'])
     for person in restaurant['choices']:
         print(f"{person['person']}: {random.choice(person['likes'])}")
@@ -15,10 +15,14 @@ try:
         print("Please supply a filename")
         sys.exit(1)
     data = json.loads(open(sys.argv[1]).read())
-    choose_food()
+    choose_food(data)
 except FileNotFoundError:
     print("Invalid filename")
     sys.exit(1)
 except json.decoder.JSONDecodeError:
-    print("Invalid JSON object")
+    print("File is invalid JSON. Assuming YAML format.")
+    import yaml
+    with open(sys.argv[1], 'r') as stream:
+        data = yaml.safe_load(stream)
+    choose_food(data)
 sys.exit(0)
